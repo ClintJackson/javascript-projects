8fc30f65e7f807c50f559489a15c6f5adcd48b24
Clint Jackson <clint.jackson09@gmail.com>
2023-12-21 21:04:05 -0600

    Added textbook notes, etc

--- data-and-variables/chapter-examples/readline-sync.js
+++ data-and-variables/chapter-examples/readline-sync.js
@@ -1,3 +1,13 @@
 const input = require('readline-sync');
 
-let info = input.question("Question text... ");
+// let info = input.question("Please enter your name: ");
+/*
+let firstName = input.question("Enter First Name: ");
+let lastName = input.question("Enter last name: ");
+
+console.log ("Welcome, " + firstName + " " + lastName + "!");
+*/
+let num1 = Number(input.question("Input a number: "));
+let num2 = Number(input.question("Input another number: "));
+//this input function treats all inputs as strings. Must use Number() function.
+console.log (num1 + num2);
