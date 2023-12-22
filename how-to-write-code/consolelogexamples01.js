7ac94c7c0d32dfab03d1e521bc269e730d4f6dbd
Clint Jackson <clint.jackson09@gmail.com>
2023-12-21 20:42:02 -0600

    added changes consistent with content being discussed

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,6 @@
-console.log('Hello, JavaScript.');
-console.log(2001);
-console.log("What","do","commas","do?");
-console.log("Does", "adding",      "space", "matter?");
-console.log('Launch' + 'Code');
+console.log('Hello, \n JavaScript.');
+console.log("\t\t\t",2001);
+console.log("What\n","\tdo\n","\t\tcommas\n","\t\t\tdo?", "\nThey separate arguments. \nUsing '+' concatenates without adding a space, but commas separate the arguments with a space");
+console.log("Does", "adding",      "space", "matter?", "\n\t\tN O. \n\t\t\texcept for within strings");
+console.log('Launch' + 'Code');//noted concatenation without space. 
 console.log("LaunchCode was founded in", 2013);
