e92b582e65d22dd4dbc22d4629f793bc5133ca3e
Clint Jackson <clint.jackson09@gmail.com>
2024-01-11 21:53:13 -0600

    Studio complete

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,47 @@
 // Initialize Variables below
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760_000;
+let shuttleMassKg = 74_842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850_000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = 100;
+let weatherStatus = "clear";
+let preparedForLiftOff = true;
 
-// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
-// add logic below to verify all astronauts are ready
+if (astronautCount <= 7 &&
+    astronautStatus === "ready" &&
+    totalMassKg < maximumMassLimit &&
+    (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) &&
+    fuelLevel === 100 &&
+    weatherStatus === "clear"){
+        console.log (`
+        All systems are a go! Initiating launch sequence...
+        ---------------------------------------------------
+        
+        Date: ${date} 
+        Time: ${time}
+        Astronaut Count: ${astronautCount}
+        Crew Mass: ${crewMassKg}kg
+        Fuel Mass: ${fuelMassKg} kg
+        Shuttle Mass: ${shuttleMassKg} kg
+        Total Mass: ${totalMassKg} kg
+        Fuel Temperature: ${fuelTempCelsius}\u00B0C
+        Weather Status: ${weatherStatus}
+        ---------------------------------------------------
+        Safe mission, astronauts!
 
-// add logic below to verify the total mass does not exceed the maximum limit of 850000
-
-// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
-// add logic below to verify the fuel level is at 100%
-
-// add logic below to verify the weather status is clear
-
-// Verify shuttle launch can proceed based on above conditions
+        "To Infinity, and Beyond!"
+            -George Washington
+        `)
+    } else {
+    console.log ("Pre-launch check failed. Aborting Launch.");
+}
