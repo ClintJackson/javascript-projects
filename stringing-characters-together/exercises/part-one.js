d78449d942489f1c8a0cc8fd96b3b61b2827886d
Clint Jackson <clint.jackson09@gmail.com>
2024-01-24 17:44:25 -0600

    completed strings

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,35 @@
 let num = 1001;
+function numToString (inputNum) {
+    return inputNum = inputNum.toString();
 
+}
+function stringNumToLength (stringNum) {
+    return numToString(stringNum).length-1;
+    
+}
+
+function stringNumDecimalLength (counterinator) {
+    counterinator = counterinator.toString();
+    let numLength;
+    if (counterinator.indexOf(".")!= -1) {
+        numLength = counterinator.length - 1;
+    } else {
+        numLength = counterinator.length;
+    }
+    return numLength;
+}
 //Returns 'undefined'.
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
+numString = num.toString();
+console.log(numString.length);
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+num = 123.45;
+console.log(stringNumToLength(num));
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+let input = require('readline-sync');
+
+let userValue = input.question("Enter a number: ");
+console.log(stringNumDecimalLength(userValue));
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,20 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+let langSlice1 = language.slice(0, 1);
+let langSlice2 = language.slice(4, 5);
+console.log(langSlice1 + langSlice2);
 //2. Without using slice(), use method chaining to accomplish the same thing.
-
+console.log(language.replace("Java", "J").replace("Script", "S"));
+let langSlice3 = language.replace("Java", "J").replace("Script", "S");
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+console.log(`The abbreviation for ${language} is ${langSlice3}`);
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
-
+console.log(language.replace("Java", "J").replace("Script", "S").toLowerCase());
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let isTitleCase = notTitleCase.replace("title", "Title").replace("case", "Case");
+console.log(isTitleCase);
