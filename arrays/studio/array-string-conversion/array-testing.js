ceb46001c74065fad13e3cb54b8312c1eae1acc5
Clint Jackson <clint.jackson09@gmail.com>
2024-01-25 16:20:07 -0600

    Studio complete

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -7,41 +7,67 @@ strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
 //2) 
 function reverseCommas() {
+	let reversedStrings = "";
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
+	for (i = 0; i < strings.length; i++) {
+		if ((strings[i].includes(',')) && !(strings[i].includes(", "))) {
+			strings[i] = strings[i].split(",").reverse().join(",");
+			reversedStrings += strings[i];
+			}
+
+		}
+
 	//TODO: 2. write the code required for this step
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
-	return output;
+	
+	return reversedStrings;
 }
 
 //3)
 function semiDash() {
-	let check;
-	let output;
-//TODO: write the code required for this step
+	let dashedStrings = "";
+	
+	for (i = 0; i < strings.length; i++) {
+		if (strings[i].includes(';')) {
+			strings[i] = strings[i].split(';').sort().join("-");
+			dashedStrings += strings[i];
+		}
+	}
 
   
-	return output;
+	return dashedStrings;
 }
 
 //4)
 function reverseSpaces() {
-	let check;
-	let output;
-  //TODO: write the code required for this step
+	let reversedSpaceString = "";
+	
+	for (i = 0; i < strings.length; i++) {
+		if (strings[i].includes(" ") && 
+		!(strings[i].includes(",") ||
+		strings[i].includes(";"))) {
+			strings[i] = strings[i].split(" ").sort().reverse().join(" ");
+			reversedSpaceString = strings[i];
+		}
+	}
 
-	return output;
+	return reversedSpaceString;
 }
 
 //5)
 function commaSpace() {
-	let check;
-	let output;
-	//TODO: write the code required for this step
+	let commaSpaceStrings = "";
+	for (i = 0; i < strings.length; i++) {
+		if (strings[i].includes(", ")) {
+			strings[i] = strings[i].split(", ").reverse().join(",");
+			commaSpaceStrings = strings[i];
+		}
+	}
+
+	
   
-	return output;
+	return commaSpaceStrings;
 }
 
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
