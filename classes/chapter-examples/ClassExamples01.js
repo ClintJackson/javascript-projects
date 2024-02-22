f524348fa91e2baf7695b314c254276b7d267d15
Clint Jackson <clint.jackson09@gmail.com>
2024-02-22 02:05:00 -0600

    clearing out backlog

--- classes/chapter-examples/ClassExamples01.js
+++ classes/chapter-examples/ClassExamples01.js
@@ -19,3 +19,17 @@ console.log(fox);
 console.log(fox.age, fox.color);
 
 //Try modifying or adding properties below.
+
+class MyFox extends Astronaut {
+  constructor(name, age, mass, isFriendly){
+    super(name, age, mass);
+      this.isFriendly = isFriendly;
+  }
+  ageIncrementer() {
+    this.age += 1;
+  }
+}
+
+let fox1 = new MyFox ("Foxy", 22, 13, true);
+
+console.log(fox1);
--- classes/chapter-examples/Inheritance.js
+++ classes/chapter-examples/Inheritance.js
@@ -21,3 +21,16 @@ class Tiger extends Panthera {
 let tigger = new Tiger();
 
 console.log(tigger);
+
+class Car {
+	constructor(make, model, year, color, mpg){
+	   this.make = make,
+	   this.model = model,
+	   this.year = year,
+	   this.color = color,
+	   this.mpg = mpg
+	}
+ }
+
+ let myCar = new Car ("Tesla", "Model S", 2019);
+ console.log(myCar);
