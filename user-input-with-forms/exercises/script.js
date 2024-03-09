3faf3131bad0040abee007a83efdd7d6c8ae589a
Clint Jackson <clint.jackson09@gmail.com>
2024-03-08 22:12:56 -0600

    finished forms exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,44 @@
 //Code Your Solution Below
+window.addEventListener("load", event =>{
+    const docFormButton = document.getElementById("formSubmit");
+
+    let testName = document.getElementById("testName"); 
+
+    let dateEntry = document.getElementById("testDate");
+
+    let rocketTypeSelect = document.getElementById("rocketType");
+
+    let rocketBoosterCount = document.getElementById("rocketBoosterCount");
+    
+    docFormButton.addEventListener("click", event => {
+
+        let alertMessage = "";
+
+        if (testName.value === "") {
+            alertMessage += "Name the test dawg\n";
+            event.preventDefault();
+        } 
+        if (dateEntry.value === ""){
+            alertMessage += "Provide a date please\n";
+            event.preventDefault();
+        }
+        if (rocketTypeSelect.value === "0"){
+            alertMessage += "Select a rocket type please\n";
+            event.preventDefault();
+        }
+
+        if (rocketBoosterCount.value === ""){
+            event.preventDefault();
+            alertMessage += "Must select number of rocket boosters\n";
+        }
+        if (!document.getElementById("noWind").checked &&
+            !document.getElementById("mild").checked &&
+            !document.getElementById("strong").checked) {
+                event.preventDefault();
+                alertMessage += "must select wind rating\n";
+            }
+        if(alertMessage.length > 0){
+            window.alert(alertMessage);
+        }
+    })
+})
